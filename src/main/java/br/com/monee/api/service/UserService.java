5d51359cb76d63983d15a574fa18cf592d208a28
Gabriel Silva <gabrielferreirasilva04@outlook.com>
2025-07-09 22:20:43 -0300

    user basico completo

---
+++ src/main/java/br/com/monee/api/service/UserService.java
@@ -0,0 +1,21 @@
+package br.com.monee.api.service;
+
+import br.com.monee.api.entity.UserEntity;
+import br.com.monee.api.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    public void save(UserEntity user){
+        this.userRepository.save(user);
+    }
+
+
+}
