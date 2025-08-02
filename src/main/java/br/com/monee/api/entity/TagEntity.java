96c8245aaa52947364e2a444265899534e4c598c
Gabriel Silva <gabrielferreirasilva04@outlook.com>
2025-08-01 22:51:44 -0300

    criação da tag

---
+++ src/main/java/br/com/monee/api/entity/TagEntity.java
@@ -0,0 +1,23 @@
+package br.com.monee.api.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.UUID;
+
+@Entity
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Table(name = "tag")
+public class TagEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String color;
+}
