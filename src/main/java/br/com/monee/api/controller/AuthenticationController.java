26a3ab836f2d08682f7a6a47b55cb7104ef73b06
gabriel_silva <gabrielferreirasilva04@outlook.com>
2025-09-04 20:10:08 -0300

    criado endpoint para a verificacao da autenticação

--- src/main/java/br/com/monee/api/controller/AuthenticationController.java
+++ src/main/java/br/com/monee/api/controller/AuthenticationController.java
@@ -12,11 +12,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -51,6 +49,14 @@ public class AuthenticationController {
         var token = this.tokenService.generateToken((UserEntity) authenticate.getPrincipal());
         this.tokenService.registerTokenInCookie(token, response);
         return  ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/verify-auth")
+    public ResponseEntity<?> verifyLogin(Authentication authentication){
+        if(authentication == null){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        return ResponseEntity.ok().build();
     }
 
 }
